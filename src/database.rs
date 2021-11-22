0386ce478999857b07a41867b45173a51e2aca55
era <the@communist.dev>
2021-11-22 18:38:24 +0000

    supporting search on database, first step

--- src/database.rs
+++ src/database.rs
@@ -1,4 +1,5 @@
-
+use sqlite;
+use sqlite::State;
 
 pub struct Data {
     text: String,
@@ -6,20 +7,74 @@ pub struct Data {
     id: String, // using string here, since the id of a table may not always be numeric
 }
 
-struct Source {
+pub struct Source {
     table: String,
     text_column: String,
     id_column: String
 }
 
-impl Source {
-    fn search(self: &Self, text: &str) -> Vec<Data> {
+pub struct DataBase {
+    sources: Vec<Source>,
+    conn: sqlite::Connection, //need to change this in the future
+}
+
+impl DataBase {
+    pub fn connect(self: &mut Self, uri: &str) {
+       let conn = sqlite::open(uri);
+
+       if let Ok(conn) = conn {
+            self.conn = conn;
+       } else {
+            panic!("Could not open a connection with SQLITE3 database");
+       }
+
+    }
+
+    pub fn add_source(self: &mut Self, source: Source) {
+        self.sources.push(source);
+    }
+
+    pub fn search(self: &Self, text: &str) -> Vec<Data> {
+        let mut results: Vec<Data> = vec![];
+        for source in &self.sources {
+            let sql = source.search_sql();
+            let mut statement = self.conn
+                                    .prepare(sql)
+                                    .unwrap();
+            
+            statement.bind(1, text);
+            
+            while let State::Row = statement.next().unwrap() {
+                let id = statement.read::<String>(0).unwrap();
+                let result_text = statement.read::<String>(1).unwrap();
+                let result = Data {table: source.table.to_string(), id: id, text: result_text};
+                results.push(result);
+            }
+        }
         vec![]
+    }
+}
+
+impl Source {
+    fn search_sql(self: &Self) -> String {
+        format!("select {id}, {text} from {table} where {table} match ?",
+                        id=self.id_column, text=self.text_column, table=self.table)
     }
 
 }
 
-pub fn search(text: &str) -> Vec<Data> {
 
-    vec![]
+#[cfg(test)]
+mod tests {
+    use crate::database;
+
+    #[test]
+    fn format_source_sql() {
+        let source = database::Source {table: "MY_TABLE".to_string(), id_column: "id".to_string(), text_column:"text".to_string()};
+
+        assert_eq!("select id, text from MY_TABLE where MY_TABLE match ?", source.search_sql())
+    }
+
 }
+
+
